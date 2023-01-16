ac58a62c080afb00d307ea7ea2d1ffd449402ba0
Ian Kirkpatrick <iankirkpatrick@metalbackgorilla.com>
2023-01-15 20:37:46 -0700

    pydnd: logging init

--- pydnd/app/dnd/repository/__init__.py
+++ pydnd/app/dnd/repository/__init__.py
@@ -3,3 +3,6 @@ from dnd.repository.dnd_class import dnd_class  # noqa: W0611
 from dnd.repository.jt_spell_to_class import jt_spell_to_class  # noqa: W0611
 from dnd.repository.source import source  # noqa: W0611
 from dnd.repository.spell import spell  # noqa: W0611
+from loguru import logger
+
+logger = logger.bind(name=__name__)
