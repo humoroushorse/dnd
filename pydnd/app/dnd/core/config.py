50d7177f7e40b4d363dc573b681188469ce408cd
Ian D Kirkpatrick <thehumoroushorse@gmail.com>
2024-04-11 12:50:53 -0600

    DND-43 Replace typing Union/Optional with '|' and None (#12)
    Co-authored-by: Ian Kirkpatrick <iankirkpatrick@metalbackgorilla.com>

--- pydnd/app/dnd/core/config.py
+++ pydnd/app/dnd/core/config.py
@@ -1,24 +1,21 @@
 """App configuration file."""
 
-# import secrets
-from typing import Optional
-
 from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
     """API settings and environment variables."""
 
-    PROJECT_NAME: Optional[str] = "Dungeons & Dragons FastAPI"
-    DESCRIPTION: Optional[str] = "Personal project for DnD"
-    VERSION: Optional[str] = "0.1.0"
-    API_V1_STR: Optional[str] = "/api/v1"
+    PROJECT_NAME: str | None = "Dungeons & Dragons FastAPI"
+    DESCRIPTION: str | None = "Personal project for DnD"
+    VERSION: str | None = "0.1.0"
+    API_V1_STR: str | None = "/api/v1"
     # SECRET_KEY: str = secrets.token_urlsafe(32)
-    SQLALCHEMY_DATABASE_URI: Optional[str] = "ostgresql://postgres:admin@localhost:5432"
-    SQLALCHEMY_TESTING_DATABASE_URI: Optional[str] = "postgresql://postgres:admin@localhost:5433"
-    LOG_LEVEL: Optional[str] = "INFO"
-    HOST: Optional[str] = "0.0.0.0"
-    PORT: Optional[int] = 8001
+    SQLALCHEMY_DATABASE_URI: str | None = "ostgresql://postgres:admin@localhost:5432"
+    SQLALCHEMY_TESTING_DATABASE_URI: str | None = "postgresql://postgres:admin@localhost:5433"
+    LOG_LEVEL: str | None = "INFO"
+    HOST: str | None = "0.0.0.0"
+    PORT: int | None = 8001
 
     class Config:
         """BaseSettings Config overrides."""
--- pydnd/app/dnd/schemas/jt_spell_to_class.py
+++ pydnd/app/dnd/schemas/jt_spell_to_class.py
@@ -1,34 +1,32 @@
 """Schemas for the jt_spell_to_class table and its CRUD operations."""
 
-from typing import Optional
-
 from pydantic import BaseModel, Field
 
 
 class JtSpellToClassBase(BaseModel):
     """Schema base fields where everything is optional."""
 
-    id: Optional[int] = Field(title="ID", description="JtSpellToClass ID (generated)")
-    source_id: Optional[int] = Field(title="Source ID", description="FK - Source ID")
-    dnd_class_id: Optional[int] = Field(title="Class ID", description="FK - Class ID")
-    spell_id: Optional[int] = Field(title="Spell ID", description="FK - Spell ID")
+    id: int | None = Field(title="ID", description="JtSpellToClass ID (generated)")
+    source_id: int | None = Field(title="Source ID", description="FK - Source ID")
+    dnd_class_id: int | None = Field(title="Class ID", description="FK - Class ID")
+    spell_id: int | None = Field(title="Spell ID", description="FK - Spell ID")
 
 
 class JtSpellToClassCreate(JtSpellToClassBase):
     """Schema for creation with required overrides and aditonal fields."""
 
-    source_name: Optional[str] = Field(title="Source Name", description="FK - Source Name")
-    dnd_class_name: Optional[str] = Field(title="Class Name", description="FK - Class Name")
-    spell_name: Optional[str] = Field(title="Spell Name", description="FK - Spell Name")
+    source_name: str | None = Field(title="Source Name", description="FK - Source Name")
+    dnd_class_name: str | None = Field(title="Class Name", description="FK - Class Name")
+    spell_name: str | None = Field(title="Spell Name", description="FK - Spell Name")
 
 
 class JtSpellToClassUpdate(JtSpellToClassBase):
     """Schema for updating model with required overrides and aditonal fields."""
 
     id: int = Field(title="ID", description="JtSpellToClass ID (generated)")
-    source_name: Optional[str] = Field(title="Source Name", description="FK - Source Name")
-    dnd_class_name: Optional[str] = Field(title="Class Name", description="FK - Class Name")
-    spell_name: Optional[str] = Field(title="Spell Name", description="FK - Spell Name")
+    source_name: str | None = Field(title="Source Name", description="FK - Source Name")
+    dnd_class_name: str | None = Field(title="Class Name", description="FK - Class Name")
+    spell_name: str | None = Field(title="Spell Name", description="FK - Spell Name")
 
 
 class JtSpellToClassResponse(JtSpellToClassBase):
